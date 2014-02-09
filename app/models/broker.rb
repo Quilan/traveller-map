bebe293b336525b830322eae3234ca4f770fe3d5
Shaun Keller <shaun@27stars.co.uk>
2014-02-09 10:53:13 +0000

    Fix for broker quantity; silence AR during method

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -14,14 +14,16 @@ class Broker
   def self.print_broker_for(system)
     puts "Broker for #{system.name}"
     puts "Goods available"
-    broker_items_for(system).each do |good|
-      puts "Item: #{good[:item].name}"
-      puts "Quantity: #{good[:qty]} tonnes"
-      puts "Purchase mod: #{good[:item].purchase_price_mod(system)}"
-      puts "Sale mod: #{good[:item].sale_price_mod(system)}"
-      puts "Base price: #{good[:item].base_price}"
-      puts "Examples: #{good[:item].examples}"
-      puts " "
+    ActiveRecord::Base.silence do
+      broker_items_for(system).each do |good|
+        puts "Item: #{good[:item].name}"
+        puts "Quantity: #{good[:quantity]} tonnes"
+        puts "Purchase mod: #{good[:item].purchase_price_mod(system)}"
+        puts "Sale mod: #{good[:item].sale_price_mod(system)}"
+        puts "Base price: #{good[:item].base_price}"
+        puts "Examples: #{good[:item].examples}"
+        puts " "
+      end
     end
     0
   end
